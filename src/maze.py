722753dd2f6765a6ac38c9deb47a868764f1cb03
Jason Edward Salim <jasonedwardsalim@gmail.com>
2025-06-19 18:40:04 +0700

    feat: create GetWalls function

--- src/maze.py
+++ src/maze.py
@@ -6,7 +6,17 @@ def CreateGrid(w,h):
         w = w+1
     if h % 2 == 0:
         h = h+1
+    # return array 2 dimensi dengan tiap elemen bernilai 1
     return np.ones((h,w),dtype=int)
 
-x = CreateGrid(25,25)
-print(x)
+def GetWalls(x,y, grid):
+    directions = [(-2,0), (2,0), (0,-2), (0,2)]
+    walls = []
+    for i,j in directions:
+        xf = x+i
+        yf = y+j
+        if (0 < xf < grid.shape[1]) and (0 < yf < grid.shape[0]) and (grid[xf][yf] == 1):
+            xt = x+i // 2
+            yt = y+j // 2
+            walls.append((xf,yf,xt,yt))
+    return walls
